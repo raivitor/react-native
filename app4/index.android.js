34b2abb54dff39ac999221260f39e9e9095a34a7
Raí Vitor <raivitor@gmail.com>
2017-07-16 18:27:42 -0300

    app4

---
+++ app4/index.android.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import ListaItens from './src/components/ListaItens';
+
+class app4 extends Component {
+  render() {
+    return (
+      <ListaItens />
+    );
+  }
+}
+
+AppRegistry.registerComponent('app4', () => app4);
