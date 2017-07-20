926374c6f682c2d9f2bbf16b4940c27701464adb
Rai Vitor <raivitor@gmail.com>
2017-07-20 14:13:54 -0300

    add navegação

--- app5/index.android.js
+++ app5/index.android.js
@@ -1,53 +1,32 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  Navigator
 } from 'react-native';
+
+import CenaPrincipal from './src/components/CenaPrincipal';
+import CenaClientes from './src/components/CenaClientes';
 
 export default class app5 extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={{ id: 'a' }}
+        renderScene={(route, navigator) => {
+          /* definir a cena com base na rota */
+          if (route.id === 'a') {
+            //exibir a cenaPrincipal
+            return (<CenaPrincipal navigator={navigator} />);
+          }
+
+          if (route.id === 'b') {
+            //exibir a cenaClientes
+            return (<CenaClientes navigator={navigator} />);
+          }
+        }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('app5', () => app5);
