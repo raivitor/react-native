57c8b37f39ce413fd05cafa85f1191c3e19ce8ab
Raí Vitor <raivitor@gmail.com>
2017-08-02 22:53:40 -0300

    adicionando transição de cenas com router-flux

--- app6/index.android.js
+++ app6/index.android.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import { Router, Scene } from 'react-native-router-flux';
+
+import Principal from './src/components/Principal';
+import SobreJogo from './src/components/SobreJogo';
+import OutrosJogos from './src/components/OutrosJogos';
 
 export default class app6 extends Component {
   render() {
     return (
+      <Router sceneStyle={{ paddingTop: 50 }}>
+        <Scene key='principal' component={Principal} initil title="Cara ou coroa" />
+        <Scene key='sobrejogo' component={SobreJogo} title="Sobre o Jogo" />
+        <Scene key='outrosjogos' component={OutrosJogos} title="Outros Jogos" />
+      </Router>
     );
   }
 }
